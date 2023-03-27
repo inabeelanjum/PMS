ae4b1de28c3758164f0039b86485ef27152c8e4b
malik15 <malik@daaneyah.com>
2023-03-27 22:11:32 +0500

    changes

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,46 +1,42 @@
+import axios from 'axios'
 
-import axios from 'axios';
+const API_URL = ' http://115.186.185.234:9010'
 
-const API_URL = ' http://115.186.185.234:9010';
+const register = async payload => {
+  try {
+    const response = await axios.post(API_URL + '/auth/signup', payload)
+    const data = response.data
 
-const register = async (payload) => {
-    try {
-        const response = await axios.post(API_URL + '/auth/signup',payload);
-        const data = response.data;
-
-        return data;
-        
-      } catch (error) {
-        console.error(error);
-      }
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
+async function login(payload) {
+  try {
+    const response = await axios.post(API_URL + '/pms/users/login', payload)
+    if (response.data.data.token) {
+      console.log(response.data.token)
+      localStorage.setItem('token', JSON.stringify(response.data.data.token))
     }
 
-
-  async function login(payload) {
-      try {
-        const response = await axios.post(API_URL + '/pms/users/login', payload);
-        if (response.data.data.token) {
-          console.log(response.data.token)
-          localStorage.setItem('token', JSON.stringify(response.data.data.token));
-        }
-
-        return response.data;
-      } catch (error) {
-        console.error(error);
-      }
-    }
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 const logout = () => {
-  localStorage.removeItem('token');
-};
+  localStorage.removeItem('token')
+}
 
 const getCurrentUser = () => {
-  return JSON.parse(localStorage.getItem('user'));
-};
+  return JSON.parse(localStorage.getItem('user'))
+}
 
 export default {
   register,
   login,
   logout,
-  getCurrentUser,
-};
+  getCurrentUser
+}
